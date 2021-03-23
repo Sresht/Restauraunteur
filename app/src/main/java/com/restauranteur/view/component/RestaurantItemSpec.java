09823e9645c8a576fc894faff0f104963fcbaa1e
Sresht Rengesh <sresht@gmail.com>
2021-03-23 16:33:37 -0600

    Get restaurants screen un up to visual spec

--- app/src/main/java/com/restauranteur/view/component/RestaurantItemSpec.java
+++ app/src/main/java/com/restauranteur/view/component/RestaurantItemSpec.java
@@ -1,15 +1,21 @@
 package com.restauranteur.view.component;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.litho.Border;
 import com.facebook.litho.Column;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
+import com.facebook.litho.Row;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
 import com.facebook.litho.annotations.Prop;
 import com.facebook.litho.fresco.FrescoImage;
 import com.facebook.litho.widget.Text;
+import com.facebook.yoga.YogaEdge;
 import com.restauranteur.R;
 import com.restauranteur.model.PopularItem;
 
@@ -18,6 +24,8 @@ import java.util.ArrayList;
 import io.reactivex.annotations.Nullable;
 
 import static com.facebook.yoga.YogaEdge.ALL;
+import static com.facebook.yoga.YogaEdge.RIGHT;
+import static com.facebook.yoga.YogaEdge.TOP;
 
 @LayoutSpec
 public class RestaurantItemSpec {
@@ -31,21 +39,41 @@ public class RestaurantItemSpec {
             @Prop final ArrayList<PopularItem> popularItems
             ) {
         Fresco.initialize(c.getApplicationContext());
-        
+
         final DraweeController controller =
                         Fresco.newDraweeControllerBuilder().setUri(coverImageUrl).build();
 
-        return Column.create(c)
+        return Row.create(c)
                 .paddingDip(ALL, 16)
-                .child(Text.create(c).text(title).textSizeSp(20))
-                .child(Text.create(c).text(cuisine).textSizeSp(12))
-                .child(Text.create(c).text(displayDistance).textSizeSp(10))
                 .child(
                         FrescoImage.create(c)
                                 .placeholderImageRes(R.drawable.placeholder_restaurant_cover)
                                 .controller(controller)
-                                .widthDip(60)
-                                .heightDip(40))
+                                .widthDip(100)
+                                .heightDip(70)
+                                .paddingDip(RIGHT, 30))
+                                .flex(1)
+                .child(
+                        Column.create(c)
+                                .child(
+                                        Text.create(c)
+                                                .text(title)
+                                                .textStyle(Typeface.BOLD)
+                                                .textSizeSp(20))
+                                .child(Text.create(c).text(cuisine).textSizeSp(14))
+                                .paddingDip(TOP, 15)
+                                .flex(1))
+                .child(
+                        Text.create(c)
+                                .text(displayDistance)
+                                .widthDip(70)
+                                .textSizeSp(14)
+                                .paddingDip(TOP, 40))
+                .border(
+                        Border.create(c)
+                        .widthDip(YogaEdge.BOTTOM, 1)
+                        .color(YogaEdge.BOTTOM, Color.LTGRAY)
+                        .build())
                 .build();
     }
 }
